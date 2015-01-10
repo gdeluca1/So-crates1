987a54b8d60440a46cef789e279d7b2e4ac4565c
gdeluca1 <gdeluca1@asu.edu>
2015-01-10 11:22:32 -0700

    Revert "Adding lost files"

--- So-crates1/src/org/usfirst/frc/team3944/robot/Robot.java
+++ So-crates1/src/org/usfirst/frc/team3944/robot/Robot.java
@@ -2,24 +2,30 @@
 package org.usfirst.frc.team3944.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.Servo;
-import edu.wpi.first.wpilibj.Timer;
 
 /**
- * The VM is configured to automatically run this class, and to call thee
+ * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the IterativeRobot
  * documentation. If you change the name of this class or the package after
  * creating this project, you must also update the manifest file in the resource
  * directory.
  */
-public class RobotTest1 extends IterativeRobot {
-	Servo servo;
+public class Robot extends IterativeRobot {
+	private TPAJoystick joystick;
+    private TPARobotDrive robotDrive;
+    private DriveTrainTester driveTrainTester;
+    
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
     public void robotInit() {
-servo = new Servo(0);
+    	joystick = new TPAJoystick(RobotMap.joystickOnePort);
+        robotDrive = new TPARobotDrive(RobotMap.frontLeftPort, RobotMap.rearLeftPort, RobotMap.frontRightPort, RobotMap.rearRightPort, joystick);
+        robotDrive.setInvertedMotor(TPARobotDrive.MotorType.kFrontRight, true);
+        robotDrive.setInvertedMotor(TPARobotDrive.MotorType.kRearRight, true);
+        
+        driveTrainTester = new DriveTrainTester(joystick, robotDrive);
     }
 
     /**
@@ -33,20 +39,19 @@ servo = new Servo(0);
      * This function is called periodically during operator control
      */
     public void teleopPeriodic() {
-    	
-    	servo.set(.88);
-    	Timer.delay(2);
-    	servo.set(.1);
-    	Timer.delay(2);
-    	
-        
+    	robotDrive.mecanumDrive_Polar();
+    }
+    
+    @Override
+    public void testInit() {
+        driveTrainTester.reset();
     }
     
     /**
      * This function is called periodically during test mode
      */
     public void testPeriodic() {
-    
+    	driveTrainTester.run();
     }
     
 }
